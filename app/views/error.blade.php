fd0639504f697e498d21c53f489a329b23c950bb
Greg <greg@poba.co>
2014-01-02 19:27:40 -0800

    Added error page. Added transparent favicon

---
+++ app/views/error.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+  <head>
+    <style>
+      #logoError {
+        width: 300px;    
+        height: 300px;
+        margin: 100px auto 0 auto;
+      }
+      .gc-error-container {
+        width: 300px;
+        height: 300px;
+        margin: 20px auto;
+        text-align: center;
+      }
+    </style>
+  </head>
+
+  <body>
+
+    <div id="logoError"></div>
+    <div class="gc-error-container">
+      We've encoutered an error.  <a href="/">Return home?</a>
+    </div>
+    <script src="/gc/js/logo.js"></script>
+    <script>
+      logo('logoError'); 
+    </script>
+  </body>
+</html>
