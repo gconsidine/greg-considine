5505f6328874be9694e5a04c4b868aa35c86a450
Greg <greg@poba.co>
2014-01-02 20:42:24 -0800

    Added new google anaytlics code, and added code to animation view template too

--- app/views/template.blade.php
+++ app/views/template.blade.php
@@ -19,8 +19,9 @@
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
       })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-      ga('create', 'UA-46796950-1', 'greg-considine.com');
+      ga('create', 'UA-46817449-1', 'greg-considine.com');
       ga('send', 'pageview');
+
     </script>
   </head>
   
