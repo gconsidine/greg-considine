d851c63218193e27d71b49436bc0f66fbb2dde37
Greg <greg@poba.co>
2014-01-09 10:27:50 -0800

    Redirecting inactive subdomains to home

--- app/routes.php
+++ app/routes.php
@@ -3,14 +3,17 @@
 Route::get('/animations/{animation}', 'AnimationController@display')
 ->where('animation', '(everywhere-usa|forest-moon|squares-and-triangles)');
 
-Route::get('/web-apps/redalytics', function() {
-  return View::make('web_apps/redalytics/index');
-});
-
+/* Route/filter asynchronous request on contact form submission */
 Route::group(['before' => 'csrf'], function () {
   Route::post('/contact', 'ContactController@sendMessage');
 });
 
+/* Unused subdomain catch-all, redirect to home */
+Route::group(['domain' => '{innactiveSubdomain}.greg-considine.com'], function () {
+  return Redirect::to('/');
+});
+
+/* Catch-all/home route */
 Route::get('/', function() {
 	return View::make('home');
 });
