990cd7b98ead4b68bfb41a0cd3b0e23646935112
Greg <greg@greg-considine.com>
2014-02-04 18:51:09 -0800

    Netanoids: get reddit JSON based on target (subreddit)

--- app/controllers/netanoids/ParseController.php
+++ app/controllers/netanoids/ParseController.php
@@ -1,10 +1,39 @@
 <?php
 
-class parseController extends BaseController {
+class ParseController extends BaseController {
   
-  /* TODO: Write reddit/YouTube API request parsers */
+  private $source;
+  private $response;
+
   public function getResponse($source) {
-    
-    return $source;
+    $this->source = $source;
+
+    if($source['source'] === 'reddit') {
+      $this->parseRedditResponse();  
+    } else if($source['source'] === 'youtube') {
+      $this->parseYoutubeResposne();
+    }
+
+    return $this->response;
+  }
+
+  private function parseRedditResponse() {
+    $reddit = new Reddit($this->source);  
+
+    // TODO: Choose first of type if it exists or return error */
+    $this->response = $reddit->fetch();
+  }
+
+  private function parseYouTubeResponse() {
+    $json = new YouTube($this->source);
+
+  }
+
+  private function getErrorMessage() {
+    return '{ "status" : "Error: Invalid JSON Response" }';
+  }
+
+  private function setResponse($response) {
+    $this->response = $response; 
   }
 }
---
+++ app/controllers/netanoids/RedditController.php
@@ -0,0 +1,4 @@
+<?php
+
+class RedditController extends BaseController {
+
